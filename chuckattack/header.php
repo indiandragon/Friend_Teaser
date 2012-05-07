b1b49206b242fa0915b1967a3087d02390d521a2
Abishek Muthian <developerdragon@gmail.com>
2012-05-07 18:14:24 +0530

    Added GNU License
    Added GNU License to my files so that you can edit and redistribute my
    files.

--- chuckattack/header.php
+++ chuckattack/header.php
@@ -1,7 +1,23 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.wzhu.devrs002.facebook.com/2008/fbml">
-<!-- Copyright indiandragon, do not redistribute this code without the permission -->
+/*Copyright 2012 indiandragon
+
+This file is part of Friend Teaser Facebook application.
+
+Friend Teaser is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Friend Teaser is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Friend Teaser.  If not, see <http://www.gnu.org/licenses/>.*/
+
 <head>
 
 <?php include "include/funcs.php"; ?>
--- chuckattack/index.php
+++ chuckattack/index.php
@@ -1,4 +1,19 @@
-<!-- Copyright indiandragon, do not redistribute this code without the permission -->
+/*Copyright 2012 indiandragon
+
+This file is part of Friend Teaser Facebook application.
+
+Friend Teaser is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Friend Teaser is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Friend Teaser.  If not, see <http://www.gnu.org/licenses/>.*/
 <?php include 'header.php'; ?>
 <title><?=FB_CONFIG_APP_NAME?></title>
 <center><img src="<?=FB_CONFIG_LOGO_IMAGE?>" id="logo"/></center>
